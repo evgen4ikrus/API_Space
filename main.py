135718c7ef41a96d60f05bf2f35575d5f94a6ae8
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-06-04 15:43:24 +0300

    Добавил функцию получения расширения ссылки

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from urllib.parse import unquote, urlparse
 
 
 def image_download(url, path):
@@ -18,15 +19,23 @@ def fetch_spacex_last_launch():
     response.raise_for_status()
     photos_links = response.json()[0]['links']['flickr_images']
     for image_number, photo_link in enumerate(photos_links):
-        image_path = f'images/spacex_{image_number}.jpg'
+        image_expansion = get_image_extension(photo_link)
+        image_path = f'images/spacex_{image_number}{image_expansion}'
         image_download(photo_link, image_path)
+
+
+def get_image_extension(url):
+    image_url = urlparse(url).path
+    image_url = unquote(image_url, encoding='utf-8', errors='replace')
+    name_image = os.path.split(image_url)[1]
+    image_expansion = os.path.splitext(name_image)[1]
+    return image_expansion
 
 
 def main():
     if not os.path.exists('images/'):
         os.makedirs('images/')
     fetch_spacex_last_launch()
- 
     
 if __name__=='__main__':
     main()
