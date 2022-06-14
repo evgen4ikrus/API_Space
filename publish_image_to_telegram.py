ec98f74c5269a7ba33939b13200c5839052df006
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-06-14 13:12:56 +0300

    Исправил опечатку

--- publish_image_to_telegram.py
+++ publish_image_to_telegram.py
@@ -11,15 +11,15 @@ from fetch_spacex_images import fetch_spacex_last_launch
 from nasa_spacex_functions import get_file_names
 
 
-def publish_image_to_telegram(image, telegram_token, telegram_chet_id):
+def publish_image_to_telegram(image, telegram_token, telegram_chat_id):
     bot = telegram.Bot(token=telegram_token)
-    bot.send_document(chat_id=telegram_chet_id,
+    bot.send_document(chat_id=telegram_chat_id,
                       document=open(f'images/{image}', 'rb'))
 
 
 def endlessly_sends_pictures_for_publication(telegram_token,
                                              publication_delay,
-                                             telegram_chet_id,
+                                             telegram_chat_id,
                                              nasa_token,
                                              images_count,
                                              photo_creation_epic_date,
@@ -35,7 +35,7 @@ def endlessly_sends_pictures_for_publication(telegram_token,
         image = random.choice(images)
         image_size = os.stat(f'images/{image}').st_size
         if image_size < 20000000:
-            publish_image_to_telegram(image, telegram_token, telegram_chet_id)
+            publish_image_to_telegram(image, telegram_token, telegram_chat_id)
             time.sleep(int(publication_delay))
         os.remove(f'images/{image}')
 
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     load_dotenv()
     telegram_token = os.getenv('TELEGRAM_TOKEN')
     publication_delay = os.getenv('PUBLICATION_DELAY', default=14400)
-    telegram_chet_id = os.getenv('TELEGRAM_CHAT_ID')
+    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
     nasa_token = os.getenv('NASA_TOKEN')
     images_count = os.getenv('IMAGES_NASA_COUNT', default=10)
     photo_creation_epic_date = os.getenv('PHOTO_CREATING_EPIC_DATE',
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     flight_number = os.getenv('SPACEX_FLIGHT_NUMBER', default=25)
     endlessly_sends_pictures_for_publication(telegram_token,
                                              publication_delay,
-                                             telegram_chet_id,
+                                             telegram_chat_id,
                                              nasa_token,
                                              images_count,
                                              photo_creation_epic_date,
